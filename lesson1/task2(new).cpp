cb281f015617ca315e87d9c34acae32e9041386d
KseniaKlimochkina <99130126+KseniaKlimochkina@users.noreply.github.com>
2022-02-06 23:24:11 +0300

    Add files via upload

---
+++ lesson1/task2(new).cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num, deg, res, sum, count;
+    char answer;
+    bool degree = true;
+    do {
+        res = 1.0;
+        sum = 0; 
+        count = 1.0;
+        cout << "Enter number: ";
+        cin >> num;
+        cout << "Enter degree: ";
+        cin >> deg;
+        for (int j = 1; j <= num; j++)
+        {
+            res = 1.0;
+            for (int i = 0; i < deg; i++)
+            {
+                res = res * j;
+            }
+            sum = sum + res;
+        }
+        cout << "Your result: ";
+        cout << sum << endl;
+        cout << "Do you want to repeat?(+ or -)" << endl;
+        cin >> answer;
+        switch (answer) {
+        case '+':
+            degree = true;
+            break;
+        case '-':
+            degree = false;
+            break;
+        }
+    } while (degree != false);
+    return 0;
+}
