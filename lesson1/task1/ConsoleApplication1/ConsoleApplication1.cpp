388bbe7cd3447c775770e980eb1fe1e19bd86329
KseniaKlimochkina <99130126+KseniaKlimochkina@users.noreply.github.com>
2022-02-14 00:44:39 +0300

    Add files via upload

---
+++ lesson1/task1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+using namespace std;
+
+float add(float, float);
+float ded(float, float);
+float divd(float, float);
+float mult(float, float);
+char infinite(char);
+
+float enterVar1(float var1) {
+    cout << "Enter first number: ";
+    cin >> var1;
+    return var1;
+}
+
+float enterVar2(float var2) {
+    cout << "Enter second number: ";
+    cin >> var2;
+    return var2;
+}
+
+char enterAction(char action) {
+    cout << "Choose action:" << endl;
+    cout << "Enter +" << endl;
+    cout << "Enter -" << endl;
+    cout << "Enter /" << endl;
+    cout << "Enter *" << endl;
+    cin >> action;
+    return action;
+}
+
+float cases(float var1, float var2, char action, char answer) {
+    var1 = enterVar1(var1);
+    var2 = enterVar2(var2);
+    action = enterAction(action);
+    bool calculator;
+    switch (action) {
+    case '+':
+        var1 = add(var1, var2);
+        break;
+    case '-':
+        var1 = ded(var1, var2);
+        break;
+    case '/':
+        var1 = divd(var1, var2);
+        break;
+    case '*':
+        var1 = mult(var1, var2);
+        break;
+    }
+    cout << var1 << endl;
+    calculator = infinite(answer);
+    return calculator;
+}
+
+float add(float var1, float var2) {
+    var1 = var1 + var2;
+    return var1;
+}
+
+float ded(float var1, float var2) {
+    var1 = var1 - var2;
+    return var1;
+}
+
+float divd(float var1, float var2) {
+    var1 = var1 / var2;
+    return var1;
+}
+
+float mult(float var1, float var2) {
+    var1 = var1 * var2;
+    return var1;
+}
+
+char infinite(char answer) {
+    bool calculator = true;
+    cout << "Do you want to repeat?(+ or -) " << endl;
+    cin >> answer;
+    switch (answer) {
+    case '+':
+        calculator = true;
+        break;
+    case '-':
+        calculator = false;
+        break;
+    }
+    return calculator;
+}
+
+void calculator() {
+    float var1 = 0, var2 = 0;
+    char action = 0, answer = 0;
+    bool calculator = 0;
+    do {
+        calculator = cases(var1, var2, action, answer);
+    } while (calculator != false);
+    return;
+}
+
+int main() {
+    calculator();
+    return 0;
+}
---
+++ lesson1/task2/task2/task2.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+using namespace std;
+
+bool infinite(char);
+void summ();
+float result(float);
+float enterNum(float);
+float enterDeg(float);
+bool cycle(float, float, char);
+float bicycle(float, int);
+
+bool infinite(char answer) {
+    cout << "Do you want to repeat?(+ or -)" << endl;
+    cin >> answer;
+    bool degree = true;
+    switch (answer) {
+    case '+':
+        degree = true;
+        break;
+    case '-':
+        degree = false;
+        break;
+    }
+    return degree;
+}
+
+void summ() {
+    float num = 0, deg = 0, res = 0, sum = 0;
+    char answer = 0;
+    bool degree = 0;
+    do {
+        degree = cycle(num, deg, answer);
+    } while (degree != false);
+    return;
+}
+
+float result(float sum) {
+    cout << "Your result: ";
+    cout << sum << endl;
+    return sum;
+}
+
+float enterNum(float num) {
+    cout << "Enter number: ";
+    cin >> num;
+    return num;
+}
+
+float enterDeg(float deg) {
+    cout << "Enter degree: ";
+    cin >> deg;
+    return deg;
+}
+
+bool cycle(float num, float deg, char answer) {
+    num = enterNum(num);
+    deg = enterDeg(deg);
+    bool degree;
+    float res;
+    float sum = 0;
+    for (int j = 1; j <= num; j++)
+    {
+        res = bicycle(deg, j);
+        sum = sum + res;
+    }
+    sum = result(sum);
+    degree = infinite(answer);
+    return degree;
+}
+
+float bicycle(float deg, int j) {
+    float res = 1.0;
+    for (int i = 0; i < deg; i++)
+    {
+        res = res * j;
+    }
+    return res;
+}
+
+int main()
+{
+    summ();
+    return 0;
+}
