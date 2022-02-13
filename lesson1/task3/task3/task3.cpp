d3f540d50ee519e7df580cf34ef92dcd7aeeb71b
KseniaKlimochkina <99130126+KseniaKlimochkina@users.noreply.github.com>
2022-02-14 01:02:42 +0300

    Add files via upload

---
+++ lesson1/task3/task3/task3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+int enter(int count) {
+    cout << "Enter number: " << endl;
+    cin >> count;
+    return count;
+}
+int devd(int count) {
+    int c1;
+    c1 = count % 10;
+    return c1;
+}
+
+int divd(int count) {
+    int c2;
+    c2 = count / 10;
+    return c2;
+ }
+
+void els(int c1, int c2) {
+    if (c1 > c2) {
+        cout << c2 << " " << c1 << endl;
+    }
+    else if (c1 < c2) {
+        cout << c1 << " " << c2 << endl;
+    }
+    else {
+        cout << "They are equal: " << c1 << " " << c2 << endl;
+    }
+    return;
+}
+
+void equal() {
+    int count = 0, c1 = 0, c2 = 0;
+    count = enter(count);
+    c1 = devd(count);
+    c2 = divd(count);
+    els(c1, c2);
+    return;
+}
+
+int main() {
+    equal();
+    return 0;
+}
